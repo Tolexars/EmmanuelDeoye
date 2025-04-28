79c07d30af6c7b322db060d0bb4a5595bd3b90e5
Tolexars <tolexar.ted@gmail.com>
2025-04-28 14:26:17 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,91 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+
+// sticky threshold
+const stickyContactThreshold = 10;
+const stickyButton = document.getElementById('sticky-contact');
+const stickyButtonCloneId = 'sticky-contact-clone';
+let stickyButtonIsCloned = false;
+
+// get current position
+function getCurrentStickyButtonPosition() {
+	//let rect = stickyButton.getBoundingClientRect();
+	return stickyButton.getBoundingClientRect();
+}
+
+// get screen width and height
+function getCurrentScreenDimensions() {
+	let viewportWidth = window.innerWidth;
+	let viewportHeight = window.innerHeight;
+	var viewportDimensions = {
+		'width': {viewportWidth},
+		'height': {viewportHeight}
+	};
+	return viewportDimensions;
+}
+
+// scroll
+document.addEventListener("scroll", function() {
+	//console.log(rect.top, stickyContactThreshold, stickyButtonActive);
+  //console.log("position", rect);
+	let rect = stickyButton.getBoundingClientRect();
+  if(rect.top <= stickyContactThreshold){
+		if(!stickyButtonIsCloned){
+			// clone
+			const clonedStickyButton = stickyButton.cloneNode(true);
+			clonedStickyButton.id = stickyButtonCloneId;
+			// add stuff
+			stickyButtonIsCloned = true;
+			stickyButton.classList.add('issticky');
+			let currentStickyButtonPosition = getCurrentStickyButtonPosition();
+			clonedStickyButton.style.top = stickyContactThreshold +'px';
+			clonedStickyButton.style.left = currentStickyButtonPosition.left+'px';
+			clonedStickyButton.style.width = currentStickyButtonPosition.width+'px';
+			clonedStickyButton.style.height = currentStickyButtonPosition.height+'px';
+			let activeClone = document.body.appendChild(clonedStickyButton);
+			let screenDimensions = getCurrentScreenDimensions();
+			let timer = setTimeout(() => activeClone.style.cssText += 'transform: translateX('+(screenDimensions.width.viewportWidth-currentStickyButtonPosition.left-currentStickyButtonPosition.width-30)+'px) translateY('+(screenDimensions.height.viewportHeight-stickyContactThreshold-currentStickyButtonPosition.height-15)+'px);', 0);
+		}
+  }else{
+		if(stickyButtonIsCloned){
+			// remove clone
+			let clonedStickyButton = document.getElementById(stickyButtonCloneId);
+      clonedStickyButton.remove();
+			// remove stuff
+			stickyButtonIsCloned = false;
+			stickyButton.classList.remove('issticky');
+			}
+  }
+});
+
+// resize
+window.addEventListener('resize', function() {
+	if(stickyButtonIsCloned){
+		let currentStickyButtonPosition = getCurrentStickyButtonPosition();
+		let clonedStickyButton = document.getElementById(stickyButtonCloneId);
+		let screenDimensions = getCurrentScreenDimensions();
+		clonedStickyButton.style.top = stickyContactThreshold +'px';
+		clonedStickyButton.style.left = currentStickyButtonPosition.left+'px';
+		clonedStickyButton.style.transform = 'translate('+(screenDimensions.width.viewportWidth-currentStickyButtonPosition.left-currentStickyButtonPosition.width-50)+'px,'+(screenDimensions.height.viewportHeight-stickyContactThreshold-currentStickyButtonPosition.height-35)+'px)';
+	}
+});
+
+
+
+const homeImage = document.getElementById('home-image-slide');
+const images = [
+    'assets/img/blue.png',
+    'assets/img/blue.png',
+];
+let currentImageIndex = 0;
+
+function changeHomeImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    homeImage.setAttribute('href', images[currentImageIndex]);
+}
+
+// Change the image every 3 seconds
+setInterval(changeHomeImage, 3000);
